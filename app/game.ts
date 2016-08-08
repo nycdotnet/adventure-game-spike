527e79bbf9050b60b88bf16507087e8ca956bb75
Steve Ognibene <steve.ognibene@outlook.com>
2016-08-07 20:30:10 -0400

    Link actually runs around!

--- app/game.ts
+++ app/game.ts
@@ -1,24 +1,52 @@
 const game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload, create, update });
-let linkSprite: Phaser.Sprite;
+
+let player: Phaser.Sprite;
+let cursors: Phaser.CursorKeys;
+let playerSpeed = 250, playerScale = 3;
 
 function preload() {
     game.load.spritesheet('linkRunning', 'images/LinkRunning.gif', 24, 28);
 }
 
 function create() {
-    linkSprite = game.add.sprite(40, 100, 'linkRunning');
-    linkSprite.animations.add('run', [0,1,2,3,4,5,6,7]);
-    linkSprite.animations.play('run', 20, true);
-    linkSprite.scale.x = 3;
-    linkSprite.scale.y = 3;
-    game.add.tween(linkSprite).to({x: game.width}, 5000, Phaser.Easing.Linear.None, true);
+    player = game.add.sprite(40, 100, 'linkRunning');
+    player.animations.add('runRight', [0,1,2,3,4,5,6,7], 30);
+    player.scale.x = playerScale;
+    player.scale.y = playerScale;
+    player.anchor.x = 0.5;
+    player.anchor.x = 0.5;
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 
 function update() {
-  if (linkSprite.x >= game.width)
-   {
-       linkSprite.x = -24;
-       game.add.tween(linkSprite).to({x: game.width}, 5000, Phaser.Easing.Linear.None, true);
-   }
+
+  player.body.velocity.setTo(0, 0);
+
+  if (cursors.left.isDown) {
+    player.body.velocity.x -= playerSpeed;
+    player.scale.x = -playerScale;
+    player.animations.play('runRight');
+  }
+  if (cursors.right.isDown) {
+    player.body.velocity.x += playerSpeed;
+    player.scale.x = playerScale;
+    player.animations.play('runRight');
+  }
+  if (cursors.up.isDown) {
+    player.body.velocity.y -= playerSpeed;
+    player.animations.play('runRight');
+  }
+  if (cursors.down.isDown) {
+    player.body.velocity.y += playerSpeed;
+    player.animations.play('runRight');
+  }
+
+  if (player.body.velocity.x === 0 && player.body.velocity.y === 0) {
+    player.animations.stop();
+    player.frame = 8;
+  }
+
 }
